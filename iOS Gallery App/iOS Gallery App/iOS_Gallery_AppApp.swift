aab236c67acff6f40c2afe59350954fdbc8dbfda
Shreyas Babu <shreyas.babu28@gmail.com>
2021-03-23 07:25:03 -0700

    initial project

---
+++ iOS Gallery App/iOS Gallery App/iOS_Gallery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Gallery_AppApp.swift
+//  iOS Gallery App
+//
+//  Created by Shreyas Babu on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Gallery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS Gallery App/iOS Gallery AppTests/iOS_Gallery_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Gallery_AppTests.swift
+//  iOS Gallery AppTests
+//
+//  Created by Shreyas Babu on 3/23/21.
+//
+
+import XCTest
+@testable import iOS_Gallery_App
+
+class iOS_Gallery_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
