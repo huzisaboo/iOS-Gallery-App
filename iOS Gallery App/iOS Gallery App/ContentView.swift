c355643dba7369e80c43f0631e50e2b040a52f0c
Shreyas Babu <shreyas.babu28@gmail.com>
2021-03-23 18:04:48 -0700

    Basic image viewing tap to fullscreen scaling added

--- iOS Gallery App/iOS Gallery App/ContentView.swift
+++ iOS Gallery App/iOS Gallery App/ContentView.swift
@@ -6,16 +6,182 @@
 //
 
 import SwiftUI
+import Photos
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class ContentView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate
+{
+
+    var myCollectionView: UICollectionView!
+    var imageArray=[UIImage]()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+        self.title = "Photo Gallary App"
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView.delegate=self
+        myCollectionView.dataSource=self
+        myCollectionView.register(PhotoCell.self, forCellWithReuseIdentifier: "Cell")
+        myCollectionView.backgroundColor=UIColor.white
+        self.view.addSubview(myCollectionView)
+        
+        myCollectionView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue)))
+        
+        GetPhotos()
+    }
+    
+    //MARK: CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
+        cell.img.image=imageArray[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
+    {
+        //print(indexPath)
+        let vc=ImagePreviewVC()
+        vc.imgArray = self.imageArray
+        vc.passedContentOffset = indexPath
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let width = collectionView.frame.width
+        if (DeviceInfo.Orientation.isPortrait)
+        {
+           return CGSize(width: width/4 - 1, height: width/4 - 1)
+        }
+        else
+        {
+           return CGSize(width: width/6 - 1, height: width/6 - 1)
+      }
+    }
+    
+    override func viewWillLayoutSubviews()
+    {
+        super.viewWillLayoutSubviews()
+        myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return 1.0
+    }
+    
+    //MARK: grab photos
+    func GetPhotos()
+    {
+        imageArray = []
+        
+        DispatchQueue.global(qos: .background).async {
+           // print("This is run on the background queue")
+            let imgManager=PHImageManager.default()
+            
+            let requestOptions=PHImageRequestOptions()
+            requestOptions.isSynchronous=true
+            requestOptions.deliveryMode = .highQualityFormat
+            
+            let fetchOptions=PHFetchOptions()
+            fetchOptions.sortDescriptors=[NSSortDescriptor(key:"creationDate", ascending: false)]
+            
+            let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            //print(fetchResult)
+            //print(fetchResult.count)
+            if (fetchResult.count > 0) {
+                for i in 0..<fetchResult.count
+                {
+                    imgManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width:500, height: 500),contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, error) in
+                        self.imageArray.append(image!)
+                    })
+                }
+            }
+            else
+            {
+                print("No Photos Avaliable")
+            }
+            print("imageArray count: \(self.imageArray.count)")
+            
+            DispatchQueue.main.async {
+                //print("This is run on the main queue, after the previous code in outer block")
+                self.myCollectionView.reloadData()
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
+class PhotoCell: UICollectionViewCell
+{
+    
+    var img = UIImageView()
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds=true
+        self.addSubview(img)
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        img.frame = self.bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+struct DeviceInfo
+{
+    struct Orientation
+    {
+        //Returns true if in LandScape orientation
+        static var isLandscape: Bool
+        {
+            get {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isLandscape
+                    : UIApplication.shared.statusBarOrientation.isLandscape
+            }
+        }
+        //Retuns true if in Portrait orientation
+        static var isPortrait: Bool
+        {
+            get
+            {
+                return UIDevice.current.orientation.isValidInterfaceOrientation
+                    ? UIDevice.current.orientation.isPortrait
+                    : UIApplication.shared.statusBarOrientation.isPortrait
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
--- iOS Gallery App/iOS Gallery App/iOS_Gallery_AppApp.swift
+++ iOS Gallery App/iOS Gallery App/iOS_Gallery_AppApp.swift
@@ -6,12 +6,50 @@
 //
 
 import SwiftUI
+import Photos
 
-@main
-struct iOS_Gallery_AppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+@UIApplicationMain
+class iOS_Gallery_AppApp: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let photos = PHPhotoLibrary.authorizationStatus()
+        if (photos == .notDetermined)
+        {
+            PHPhotoLibrary.requestAuthorization({status in
+                if (status == .authorized)
+                {
+                    self.callContentView()
+                }
+                else
+                {
+                    let alert = UIAlertController(title: "Access Denied", message: "Need access to photo library", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+                }
+            })
         }
+        else if (photos == .authorized)
+        {
+            callContentView()
+        }
+        
+        return true
+    }
+    
+    func callContentView()
+    {
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            if let window = self.window
+            {
+                window.backgroundColor = UIColor.white
+                let nav = UINavigationController()
+                let mainView = ContentView()
+                nav.viewControllers = [mainView]
+                window.rootViewController = nav
+                window.makeKeyAndVisible()
+            }
+        })
     }
 }
